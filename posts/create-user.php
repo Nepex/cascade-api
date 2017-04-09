7541d1cea21b7734713c5b12e3f61e1fa690f181
nepex <thechump@hotmail.com>
2017-04-09 00:29:19 -0500

    create user php

--- posts/create-user.php
+++ posts/create-user.php
@@ -8,13 +8,34 @@
     $email = $_GET['email'];
     $password = $_GET['password'];
 
-    if ($stmt = $mysqli->prepare("INSERT INTO `users` (username, password, email) VALUES (?, ?, ?)")) {
-        $stmt->bind_param('sss', $username, $password, $email);
-        $stmt->execute();
-        
-        echo 'success!';
+    $searchTakenNames = mysqli_query($mysqli, "SELECT * FROM users WHERE username='$username'");
+    $nameMatches = mysqli_num_rows($searchTakenNames);
+    $searchTakenEmails = mysqli_query($mysqli, "SELECT * FROM users WHERE email='$email'");
+    $emailMatches = mysqli_num_rows($searchTakenEmails);
 
-        $stmt->close();
+    if ($nameMatches != 0) {
+        echo 'username taken';
+    } else if ($emailMatches != 0) {
+        echo 'email taken';
+    } else {
+        // create user
+        if ($stmt = $mysqli->prepare("INSERT INTO `users` (username, password, email) VALUES (?, ?, ?)")) {
+            $hash = password_hash($password, PASSWORD_DEFAULT);
+            $stmt->bind_param('sss', $username, $hash, $email);
+            $stmt->execute();
+            
+            // create inventory
+            $inventoryCreate = "INSERT INTO `inventory` (username) VALUES (?)";
+            if ($stmt = $mysqli->prepare($inventoryCreate)) {
+                $stmt->bind_param('s', $username);
+                $stmt->execute();
+                $stmt->close();
+            }
+
+            echo 'success';
+
+            $stmt->close();
+        }
     }
 
     $mysqli->close();
