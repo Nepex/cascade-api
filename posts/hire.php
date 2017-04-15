ad1bb1d8922e5b848607d77ac666837c7d953979
nepex <thechump@hotmail.com>
2017-04-14 23:06:45 -0500

    remove party slots

--- posts/hire.php
+++ posts/hire.php
@@ -34,28 +34,21 @@
             if ($result->num_rows) {
                 while($row = $result->fetch_assoc()) {
                     $username = $row['username'];
-                    $partySlotsAvailable = $row['party_slots'] - 1;
                 }
             }
 
-            // remove available party slot
-            if ($stmt = $mysqli->prepare("UPDATE `users` SET party_slots = ? WHERE id = $userId")) {
-                $stmt->bind_param('i', $partySlotsAvailable);
+            // create party member
+            if ($stmt = $mysqli->prepare("INSERT INTO `party` (owner, name, job, sprite, knight_lvl, mage_lvl, priest_lvl,
+            experience, experience_needed, current_hp, hp, current_mp, mp) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")) {
+                $stmt->bind_param('sssssiiiiiiii', $username, $name, $job, $sprite, $jobLevels, $jobLevels, $jobLevels, $experience, $experienceNeeded, $currentHp, $hp, $currentMp, $mp);
                 $stmt->execute();
-                
-                // create party member
-                if ($stmt = $mysqli->prepare("INSERT INTO `party` (owner, name, job, sprite, knight_lvl, mage_lvl, priest_lvl,
-                experience, experience_needed, current_hp, hp, current_mp, mp) 
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")) {
-                    $stmt->bind_param('sssssiiiiiiii', $username, $name, $job, $sprite, $jobLevels, $jobLevels, $jobLevels, $experience, $experienceNeeded, $currentHp, $hp, $currentMp, $mp);
-                    $stmt->execute();
-                    $stmt->close();
-                }
-
-                echo 'success';
-
                 $stmt->close();
-            } 
+            }
+            
+            echo 'success';
+
+            $stmt->close();
         }
     }
 
