cbedbdc4fedd0d5a3c9c6ba933e32f421630717d
nepex <thechump@hotmail.com>
2017-05-20 21:28:24 -0500

    combat states

---
+++ puts/set-in-combat.php
@@ -0,0 +1,28 @@
+<?php
+include '../connection.php';
+include '../jwt-helper.php';
+
+header('Access-Control-Allow-Origin: http://localhost:4200');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+
+$headers = apache_request_headers();
+$sentToken = $headers['Authorization'];
+$combatState = 'true';
+
+if ($sentToken) {
+    $decoded = JWT::decode($sentToken, '8725309');
+    
+    $userId = $decoded->id;
+    
+    if ($stmt = $mysqli->prepare("UPDATE users SET combat = ? WHERE id = ?")) {
+        $stmt->bind_param('si', $combatState, $userId);
+        $stmt->execute();
+        $stmt->close();
+    }
+    
+    echo 'success';
+}
+
+$mysqli->close();
+?>
